bb3ead338f885abff5aa22914a3259143c1ce73f
mehdi <mehdi.shokohifar@gmail.com>
2023-12-29 23:39:15 +0330

    clean comments

--- mh_test.go
+++ mh_test.go
@@ -12,48 +12,47 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-
 type User struct {
 	ID       *primitive.ObjectID `json:"id,omitempty"   bson:"_id,omitempty"`
 	Password string              `json:"password,omitempty"   bson:"password"`
 	Username string              `json:"username"   bson:"userName"`
 	Status   string              `json:"status" bson:"status"`
 }
-var db *mongo.Client
-func NewMongo[T any](ctx context.Context, colName string, model T) MongoContainer[T] {
-	if db==nil{
 
-		db=New("mongo_rnj_rw#1","mongodb://localhost:27018/rainjoy?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false")
+var db *mongo.Client
+
+func NewMongo[T any](ctx context.Context, colName string, model T) MongoContainer[T] {
+	if db == nil {
+		db = New("mongo_rnj_rw#1", "mongodb://localhost:27018/rainjoy?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false")
 	}
 	m := MongoContainer[T]{}
 	m.Model = model
 	m.Ctx = ctx
 	m.DatabaseName = "rainjoy"
-	m.Connection ,_= Holder.Read("mongo_rnj_rw#1")
+	m.Connection= db
 	m.CollectionName = colName
 	return m
 }
 func TestMongoHelper(t *testing.T) {
 
-db:=NewMongo(context.TODO(),"test",User{})
-finded,err:=db.FindAll(&bson.D{{"userName","mehdi"}})
-if err!=nil{
-	fmt.Println(err)
-}
-users,_:=json.Marshal(finded)
-fmt.Println(string(users))
-tr:=StartTransaction("mongo_rnj_rw#1",context.Background())
-res,err :=tr.EndTransaction(func(sessionContext mongo.SessionContext) (result interface{}, err error) {
-	db:=NewMongo(sessionContext,"test",User{Username: "mehdi",Password: "1234"})
-	db.Insert()
-	db2:=NewMongo(sessionContext,"test2",User{Username: "mate",Password: "1234"})
-	res,err:= db2.Insert()
-	if err==nil{
-		return res,errors.New("Abort transaction")
+	db := NewMongo(context.TODO(), "test", User{})
+	finded, err := db.FindAll(&bson.D{{"userName", "mehdi"}})
+	if err != nil {
+		fmt.Println(err)
 	}
-	return res,err
-})
+	users, _ := json.Marshal(finded)
+	fmt.Println(string(users))
+	tr := StartTransaction("mongo_rnj_rw#1", context.Background())
+	res, err := tr.EndTransaction(func(sessionContext mongo.SessionContext) (result interface{}, err error) {
+		db := NewMongo(sessionContext, "test", User{Username: "mehdi", Password: "1234"})
+		db.Insert()
+		db2 := NewMongo(sessionContext, "test2", User{Username: "mate", Password: "1234"})
+		res, err := db2.Insert()
+		if err == nil {
+			return res, errors.New("Abort transaction")
+		}
+		return res, err
+	})
 
-
-fmt.Println(res)
+	fmt.Println(res)
 }
--- mongo.go
+++ mongo.go
@@ -14,16 +14,14 @@ import (
 
 var ErrorInsertFailed = errors.New("insert failed with no error")
 
-
-
 type Transaction struct {
 	connection *mongo.Client
 	ctx        context.Context
 }
 
-func StartTransaction(Id string,ctx context.Context) Transaction {
+func StartTransaction(Id string, ctx context.Context) Transaction {
 	tr := Transaction{}
-	tr.ctx =ctx
+	tr.ctx = ctx
 	tr.connection, _ = Holder.Read(Id)
 	return tr
 }
@@ -49,14 +47,11 @@ func (t *Transaction) EndTransaction(f func(sessionContext mongo.SessionContext)
 }
 
 func (m *MongoContainer[T]) GetCollection() *mongo.Collection {
-	// conn,_ := Holder.Read(id)
-	// conn := mongoDB.GetConnection()
 	var collection *mongo.Collection
 	if cs, ok := m.Ctx.(mongo.SessionContext); ok {
 		collection = cs.Client().Database(m.DatabaseName).Collection(m.CollectionName)
 	} else {
 		collection = m.Connection.Database(m.DatabaseName).Collection(m.CollectionName)
-
 	}
 	return collection
 }
@@ -69,23 +64,7 @@ type MongoContainer[T any] struct {
 	CollectionName string
 }
 
-// func (m *MongoContainer[T]) ConnectionManager(Op func(ctx context.Context, collection *mongo.Collection) (interface{}, error)) (interface{}, error) {
-// 	return func() (interface{}, error) {
-// 		var collection *mongo.Collection
-// 		if cs, ok := m.Ctx.(mongo.SessionContext); ok {
-// 			collection = cs.Client().Database(m.DatabaseName).Collection(m.CollectionName)
-// 		} else {
-// 			collection = m.Connection.Database(m.DatabaseName).Collection(m.CollectionName)
-
-// 		}
-// 		return Op(m.Ctx, collection)
-// 	}()
-// }
-
 func (m *MongoContainer[T]) Insert() (result *mongo.InsertOneResult, err error) {
-
-	// return m.ConnectionManager(func(ctx context.Context, collection *mongo.Collection) (interface{}, error) {
-
 	insertResult, err := m.GetCollection().InsertOne(m.Ctx, m.Model)
 	if err != nil {
 
@@ -94,14 +73,12 @@ func (m *MongoContainer[T]) Insert() (result *mongo.InsertOneResult, err error) 
 	if insertResult == nil {
 		return nil, ErrorInsertFailed
 	}
-
 	return insertResult, err
-	// })
+
 }
 
 func (m *MongoContainer[T]) Update(newValue T, findCondition bson.D) (result *mongo.UpdateResult, err error) {
 
-	// return m.ConnectionManager(func(ctx context.Context, collection *mongo.Collection) (interface{}, error) {
 	updateFilter := bson.D{{Key: "$set", Value: newValue}}
 	updatedResult, err := m.GetCollection().UpdateOne(m.Ctx, findCondition, updateFilter)
 	if err != nil {
@@ -112,10 +89,8 @@ func (m *MongoContainer[T]) Update(newValue T, findCondition bson.D) (result *mo
 		return nil, mongo.ErrNoDocuments
 	}
 	return updatedResult, err
-	// })
 }
 func (m *MongoContainer[T]) UpdateMany(filter interface{}, update interface{}, options ...*options.UpdateOptions) (result *mongo.UpdateResult, err error) {
-	// return m.ConnectionManager(func(ctx context.Context, collection *mongo.Collection) (interface{}, error) {
 	updatedResult, err := m.GetCollection().UpdateMany(m.Ctx, filter, update, options...)
 	if err != nil {
 		return nil, err
@@ -123,14 +98,11 @@ func (m *MongoContainer[T]) UpdateMany(filter interface{}, update interface{}, o
 	}
 	if updatedResult == nil {
 		return nil, mongo.ErrNoDocuments
-
 	}
 	return updatedResult, nil
-	// })
 }
 
 func (m *MongoContainer[T]) FindOne(query *bson.D, options ...*options.FindOneOptions) (*T, error) {
-	// return m.ConnectionManager(func(ctx context.Context, collection *mongo.Collection) (interface{}, error) {
 	one := m.GetCollection().FindOne(m.Ctx, query, options...)
 	err := one.Decode(&m.Model)
 	if err == nil {
@@ -138,17 +110,11 @@ func (m *MongoContainer[T]) FindOne(query *bson.D, options ...*options.FindOneOp
 	}
 
 	return nil, err
-	// })
+
 }
 
 func (m *MongoContainer[T]) FindAll(query *bson.D, opts ...*options.FindOptions) ([]*T, error) {
-	// var collection *mongo.Collection
-	// if cs, ok := m.Ctx.(mongo.SessionContext); ok {
-	// 	collection = cs.Client().Database(m.DatabaseName).Collection(m.CollectionName)
-	// } else {
-	// 	collection = m.Connection.Database(m.DatabaseName).Collection(m.CollectionName)
 
-	// }
 	collection := m.GetCollection()
 	results := make([]*T, 0)
 	cur, err := collection.Find(m.Ctx, query, opts...)
@@ -175,21 +141,11 @@ func (m *MongoContainer[T]) FindAll(query *bson.D, opts ...*options.FindOptions)
 
 // CountDocuments returns total documents founded by query.
 func (m *MongoContainer[T]) CountDocuments(query *bson.D) (interface{}, error) {
-	// return m.ConnectionManager(func(ctx context.Context, collection *mongo.Collection) (interface{}, error) {
 	return m.GetCollection().CountDocuments(m.Ctx, query)
-	// })
-
 }
 
 // Aggregate as you know aggregate mongo pipeline!
 func (m *MongoContainer[T]) Aggregate(pipeline mongo.Pipeline, opts ...*options.AggregateOptions) (result []T, err error) {
-	// var collection *mongo.Collection
-	// if cs, ok := m.Ctx.(mongo.SessionContext); ok {
-	// 	collection = cs.Client().Database(m.DatabaseName).Collection(m.CollectionName)
-	// } else {
-	// 	collection = m.Connection.Database(m.DatabaseName).Collection(m.CollectionName)
-
-	// }
 	collection := m.GetCollection()
 	aggResult, err := collection.Aggregate(m.Ctx, pipeline, opts...)
 	if err != nil {
@@ -217,36 +173,29 @@ func (m *MongoContainer[T]) Aggregate(pipeline mongo.Pipeline, opts ...*options.
 }
 
 func (m *MongoContainer[T]) FindByID(id interface{}) (*T, error) {
-	// return m.ConnectionManager(func(ctx context.Context, collection *mongo.Collection) (interface{}, error) {
-		one := m.GetCollection().FindOne(m.Ctx, &bson.D{{Key: "_id", Value: id}})
-		err := one.Err()
-		if err != nil {
+	one := m.GetCollection().FindOne(m.Ctx, &bson.D{{Key: "_id", Value: id}})
+	err := one.Err()
+	if err != nil {
 
-			return nil, err
-		}
-		err = one.Decode(m.Model)
-		return &m.Model, err
-	// })
+		return nil, err
+	}
+	err = one.Decode(m.Model)
+	return &m.Model, err
 }
 
 func (m *MongoContainer[T]) DeleteOne(b *bson.D, opts ...*options.DeleteOptions) (result *mongo.DeleteResult, err error) {
-	// return m.ConnectionManager(func(ctx context.Context, collection *mongo.Collection) (interface{}, error) {
-		delete, err := m.GetCollection().DeleteOne(m.Ctx, b, opts...)
-		if err != nil {
-			return nil, err
-		}
+	delete, err := m.GetCollection().DeleteOne(m.Ctx, b, opts...)
+	if err != nil {
+		return nil, err
+	}
 
-		return delete, nil
-	// })
+	return delete, nil
 }
 
 func (m *MongoContainer[T]) DeleteMany(b *bson.D, opts ...*options.DeleteOptions) (result *mongo.DeleteResult, err error) {
-	// return m.ConnectionManager(func(ctx context.Context, collection *mongo.Collection) (interface{}, error) {
-		delCount, err := m.GetCollection().DeleteMany(m.Ctx, b, opts...)
-		if err != nil {
-			return nil, err
-		}
-
-		return delCount, nil
-	// })
+	delCount, err := m.GetCollection().DeleteMany(m.Ctx, b, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return delCount, nil
 }
